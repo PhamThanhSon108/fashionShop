f712659bbc7c8afa3a64a0c242117977992b0dcb
vanlong <buoivip199@gmail.com>
2022-06-17 23:15:22 +0700

    fomat cart

--- user/src/screens/CartScreen.js
+++ user/src/screens/CartScreen.js
@@ -1,136 +1,156 @@
-import React, { useEffect, useState } from "react";
-import Header from "./../components/Header";
-import { Link } from "react-router-dom";
-import { useDispatch, useSelector } from "react-redux";
-import { addToCart, clearFromCart, listCart, removefromcart } from "./../Redux/Actions/cartActions";
+import React, { useEffect, useState } from 'react';
+import Header from './../components/Header';
+import { Link } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { addToCart, clearFromCart, listCart, removefromcart } from './../Redux/Actions/cartActions';
 
 const CartScreen = ({ match, location, history }) => {
-  window.scrollTo(0, 0);
-  const dispatch = useDispatch();
-  const productId = match.params.id;
-  const qty = location.search ? Number(location.search.split("=")[1]) : 1;
+    window.scrollTo(0, 0);
+    const dispatch = useDispatch();
+    const productId = match.params.id;
+    const qty = location.search ? Number(location.search.split('=')[1]) : 1;
 
-  const cart = useSelector((state) => state.cart);
-  const { cartItems } = cart;
+    const cart = useSelector((state) => state.cart);
+    const { cartItems } = cart;
 
-  const cartDel = useSelector((state) => state.cartDelete);
-  const { loading: loa, success: suc, mesage: mes } = cartDel;
+    const cartDel = useSelector((state) => state.cartDelete);
+    const { loading: loa, success: suc, mesage: mes } = cartDel;
 
-  const cartCreate = useSelector(state => state.cartCreate);
-  const { loading: loadingCreate, success: successCreate } = cartCreate
-  const total = cartItems ? cartItems.reduce((a, i) => a + i.qty * i.price, 0).toFixed(2) : 0;
-  const userLogin = useSelector((state) => state.userLogin);
-  const { userInfo } = userLogin;
-  // useEffect(() => {
-  //   if (productId) {
-  //     console.log("1use")
-  //     // dispatch(addToCart(productId, qty, userInfo._id))
+    const cartCreate = useSelector((state) => state.cartCreate);
+    const { loading: loadingCreate, success: successCreate } = cartCreate;
+    const total = cartItems ? cartItems.reduce((a, i) => a + i.qty * i.price, 0).toFixed(2) : 0;
+    const userLogin = useSelector((state) => state.userLogin);
+    const { userInfo } = userLogin;
+    // useEffect(() => {
+    //   if (productId) {
+    //     console.log("1use")
+    //     // dispatch(addToCart(productId, qty, userInfo._id))
 
-  //   }
+    //   }
 
-  //   // }, [dispatch, productId, qty]);
-  // }, [dispatch, productId, qty]);
-  const checkOutHandler = () => {
-    history.push("/login?redirect=shipping");
-  };
-  useEffect(() => {
-    dispatch(listCart());
-  }, [suc, successCreate]);
+    //   // }, [dispatch, productId, qty]);
+    // }, [dispatch, productId, qty]);
+    const checkOutHandler = () => {
+        history.push('/login?redirect=shipping');
+    };
+    useEffect(() => {
+        dispatch(listCart());
+    }, [suc, successCreate]);
 
-  const removeFromCartHandle = (id) => {
-    console.log(id)
-    dispatch(removefromcart(id));
-  };
-  return (
-    <>
-      <Header />
-      {/* Cart */}
-      <div className="container">
-        {cartItems.length === 0 ? (
-          <div className=" alert alert-info text-center mt-3">
-            Your cart is empty
-            <Link
-              className="btn btn-success mx-5 px-5 py-3"
-              to="/"
-              style={{
-                fontSize: "12px",
-              }}
-            >
-              SHOPPING NOW
-            </Link>
-          </div>
-        ) : (
-          <>
-            <div className=" alert alert-info text-center mt-3">
-              Total Cart Products
-              <Link className="text-success mx-2" to="/cart">
-                ({cartItems?.length ?? 0})
-              </Link>
+    const removeFromCartHandle = (id) => {
+        console.log(id);
+        dispatch(removefromcart(id));
+    };
+    return (
+        <>
+            <Header />
+            {/* Cart */}
+            <div className="container">
+                {cartItems.length === 0 ? (
+                    <div className=" alert alert-info text-center mt-3">
+                        <div style={{ display: 'flex', flexDirection: 'column' }}>
+                            <img
+                                style={{ width: '100px', height: '100px', margin: '0 auto' }}
+                                src="https://deo.shopeemobile.com/shopee/shopee-pcmall-live-sg/cart/9bdd8040b334d31946f49e36beaf32db.png"
+                            ></img>
+                            Your shopping cart is empty
+                        </div>
+                        <Link
+                            className="btn btn-success mx-5 px-5 py-3"
+                            to="/"
+                            style={{
+                                fontSize: '12px',
+                            }}
+                        >
+                            SHOPPING NOW
+                        </Link>
+                    </div>
+                ) : (
+                    <>
+                        <div className="backTo" style={{ paddingTop: '10px' }}>
+                            <Link to="/" className="col-md-6 ">
+                                <i class="fas fa-undo" style={{ paddingRight: '5px' }}></i>
+                                Back To Shop
+                            </Link>
+                        </div>
+                        <div className=" alert alert-info text-center mt-3">
+                            Total Cart Products
+                            <Link className="text-success mx-2" to="/cart">
+                                ({cartItems?.length ?? 0})
+                            </Link>
+                        </div>
+                        {/* cartiterm */}
+                        <div className="cart-scroll">
+                            {cartItems?.map((item) => (
+                                <div className="cart-iterm row">
+                                    {/*check box */}
+                                    <div className="col-md-1 cart-check">
+                                        <input type="checkbox"></input>
+                                    </div>
+                                    {/* hết hàng */}
+                                    {/* <div className="col-md-1 cart-check">
+                                    <span className="span" style={{ fontSize: '12px', color: 'red' }}>
+                                        Hết hàng
+                                    </span>
+                                </div> */}
+                                    <div className="cart-image col-md-2">
+                                        <img src={item.image} alt={item.name} />
+                                    </div>
+                                    <div className="cart-text col-md-4 d-flex align-items-center">
+                                        <Link to={`/products/${item.product}`}>
+                                            <h4>{item.name}</h4>
+                                        </Link>
+                                    </div>
+                                    <div className="cart-qty col-md-2 col-sm-5 mt-3 mt-md-0 d-flex flex-column justify-content-center">
+                                        <h6>QUANTITY</h6>
+                                        <select
+                                            value={item.qty}
+                                            onChange={(e) => {
+                                                dispatch(addToCart(item.product, e.target.value, userInfo._id));
+                                            }}
+                                        >
+                                            {[...Array(item.countInStock).keys()].map((x) => (
+                                                <option key={x + 1} value={x + 1}>
+                                                    {x + 1}
+                                                </option>
+                                            ))}
+                                        </select>
+                                    </div>
+                                    <div className="cart-price mt-3 mt-md-0 col-md-2 align-items-sm-end align-items-start  d-flex flex-column justify-content-center col-sm-7">
+                                        <h6>PRICE</h6>
+                                        <h4>${item.price}</h4>
+                                    </div>
+                                    <div
+                                        className=" col-md-1 delete-cart"
+                                        onClick={() => {
+                                            removeFromCartHandle(item.product);
+                                        }}
+                                        style={{ display: 'flex', justifyContent: 'right', cursor: 'pointer' }}
+                                    >
+                                        Xóa
+                                    </div>
+                                </div>
+                            ))}
+                        </div>
+
+                        {/* End of cart iterms */}
+                        <hr />
+                        <div className="cart-buttons d-flex align-items-center row">
+                            <div className="total col-md-6">
+                                <span className="sub">total:</span>
+                                <span className="total-price">${total}</span>
+                            </div>
+                            {total > 0 && (
+                                <div className="col-md-6 d-flex justify-content-md-end mt-3 mt-md-0">
+                                    <button onClick={checkOutHandler}>Checkout</button>
+                                </div>
+                            )}
+                        </div>
+                    </>
+                )}
             </div>
-            {/* cartiterm */}
-            {cartItems?.map((item) => (
-              <div className="cart-iterm row">
-                <div
-                  onClick={() => {
-
-                    removeFromCartHandle(item.product)
-                  }}
-                  className="remove-button d-flex justify-content-center align-items-center"
-                >
-                  <i className="fas fa-times"></i>
-                </div>
-                <div className="cart-image col-md-3">
-                  <img src={item.image} alt={item.name} />
-                </div>
-                <div className="cart-text col-md-5 d-flex align-items-center">
-                  <Link to={`/products/${item.product}`}>
-                    <h4>{item.name}</h4>
-                  </Link>
-                </div>
-                <div className="cart-qty col-md-2 col-sm-5 mt-md-5 mt-3 mt-md-0 d-flex flex-column justify-content-center">
-                  <h6>QUANTITY</h6>
-                  <select
-                    value={item.qty}
-                    onChange={(e) => {
-                      dispatch(addToCart(item.product, e.target.value, userInfo._id))
-                    }
-                    }
-                  >
-                    {[...Array(item.countInStock).keys()].map((x) => (
-                      <option key={x + 1} value={x + 1}>
-                        {x + 1}
-                      </option>
-                    ))}
-                  </select>
-                </div>
-                <div className="cart-price mt-3 mt-md-0 col-md-2 align-items-sm-end align-items-start  d-flex flex-column justify-content-center col-sm-7">
-                  <h6>PRICE</h6>
-                  <h4>${item.price}</h4>
-                </div>
-              </div>
-            ))}
-
-            {/* End of cart iterms */}
-            <div className="total">
-              <span className="sub">total:</span>
-              <span className="total-price">${total}</span>
-            </div>
-            <hr />
-            <div className="cart-buttons d-flex align-items-center row">
-              <Link to="/" className="col-md-6 ">
-                <button>Continue To Shopping</button>
-              </Link>
-              {total > 0 && (
-                <div className="col-md-6 d-flex justify-content-md-end mt-3 mt-md-0">
-                  <button onClick={checkOutHandler}>Checkout</button>
-                </div>
-              )}
-            </div>
-          </>
-        )}
-      </div>
-    </>
-  );
+        </>
+    );
 };
 
 export default CartScreen;
