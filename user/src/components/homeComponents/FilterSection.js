1b4e8b48ff649d9e249705520adbd5ac5f6f87f1
vanlong <buoivip199@gmail.com>
2022-06-23 16:21:36 +0700

    format ui user filter ui sider admin

--- user/src/components/homeComponents/FilterSection.js
+++ user/src/components/homeComponents/FilterSection.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { ListCategory } from '../../Redux/Actions/categoryActions';
 import { Link } from 'react-router-dom';
+import isEmpty from 'validator/lib/isEmpty';
 import Rating from './Rating';
 
 export default function FilterSection(props) {
@@ -14,11 +15,47 @@ export default function FilterSection(props) {
     const [minPrice, setMinPrice] = useState('');
     const [maxPrice, setMaxPrice] = useState('');
 
+    const [checkId, setCheckId] = useState('');
     if (rating) {
         props.setRating(rating);
     }
 
+    //xủ lí logic check from
+    const [price, SetPrice] = useState({});
+    const checkPrice = () => {
+        const msg = {};
+        if (isEmpty(minPrice)) {
+            msg.name = 'Please input your price';
+        } else {
+            if (minPrice < 0) {
+                msg.name = 'Please enter the positive value';
+            } else {
+                if (isNaN(minPrice)) {
+                    msg.name = 'Please enter the number';
+                }
+            }
+        }
+        if (isEmpty(maxPrice)) {
+            msg.name = 'Please input your price';
+        } else {
+            if (minPrice < 0) {
+                msg.name = 'Please enter the positive value';
+            } else {
+                if (isNaN(minPrice)) {
+                    msg.name = 'Please enter the number';
+                } else {
+                    if (Number(minPrice) > Number(maxPrice)) {
+                        msg.name = 'MinPrice is smaller MaxPrice';
+                    }
+                }
+            }
+        }
+        SetPrice(msg);
+        if (Object.keys(msg).length > 0) return false;
+        return true;
+    };
     const ApplyHandler = () => {
+        if (!checkPrice()) return;
         props.setMinPrice(minPrice);
         props.setMaxPrice(maxPrice);
     };
@@ -34,8 +71,18 @@ export default function FilterSection(props) {
                 </div>
                 <ul className="Category-section__list">
                     {categories.map((category) => (
-                        <li className="Category-section__li">
-                            <Link to={`/category/${category._id}`}>{category.name}</Link>
+                        <li
+                            className="Category-section__li"
+                            onClick={() => {
+                                setCheckId(category._id);
+                            }}
+                        >
+                            <Link
+                                style={checkId === category._id ? { color: 'red' } : {}}
+                                to={`/category/${category._id}`}
+                            >
+                                {category.name}
+                            </Link>
                         </li>
                     ))}
                 </ul>
@@ -53,6 +100,7 @@ export default function FilterSection(props) {
                         <label>-</label>
                         <input type="text" placeholder="$Max" onChange={(e) => setMaxPrice(e.target.value)}></input>
                     </div>
+                    <p style={{ fontSize: '14px', color: 'red' }}>{price.name}</p>
                     <button className="distance-price__submit" onClick={ApplyHandler}>
                         APPLY
                     </button>
@@ -72,7 +120,7 @@ export default function FilterSection(props) {
                                     setRating(e.target.value);
                                 }}
                             ></input>
-                            <label for="five">
+                            <label for="five" className={rating === '5' ? 'rating-color' : ' '}>
                                 <Rating value="5"></Rating>
                             </label>
                         </div>
@@ -88,7 +136,7 @@ export default function FilterSection(props) {
                                     setRating(e.target.value);
                                 }}
                             ></input>
-                            <label for="four">
+                            <label for="four" className={rating === '4' ? 'rating-color' : ' '}>
                                 <Rating value="4" text={'& up'}></Rating>
                             </label>
                         </div>
@@ -104,7 +152,7 @@ export default function FilterSection(props) {
                                     setRating(e.target.value);
                                 }}
                             ></input>
-                            <label for="three">
+                            <label for="three" className={rating === '3' ? 'rating-color' : ' '}>
                                 <Rating value="3" text={'& up'}></Rating>
                             </label>
                         </div>
@@ -120,7 +168,7 @@ export default function FilterSection(props) {
                                     setRating(e.target.value);
                                 }}
                             ></input>
-                            <label for="two">
+                            <label for="two" className={rating === '2' ? 'rating-color' : ' '}>
                                 <Rating value="2" text={'& up'}></Rating>
                             </label>
                         </div>
@@ -136,11 +184,18 @@ export default function FilterSection(props) {
                                     setRating(e.target.value);
                                 }}
                             ></input>
-                            <label for="one">
+                            <label for="one" className={rating === '1' ? 'rating-color' : ''}>
                                 <Rating value="1" text={'& up'}></Rating>
                             </label>
                         </div>
                     </div>
+                </div>
+                <div className="" display={{ display: 'flex', alignItems: 'center' }}>
+                    <button className="distance-price__submit" onClick={ApplyHandler}>
+                        <Link className="navbar-brand" to="/" style={{ fontSize: '0.85rem', color: '#fff' }}>
+                            DELETE ALL FILTER
+                        </Link>
+                    </button>
                 </div>
             </div>
         </div>
